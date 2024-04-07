cf4ed84110ea9398fd2579256c3e5be63e518aa0
adis21104 <210305124162@paruluniversity.ac.in>
2024-04-07 18:22:28 +0530

    minor change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="container relative">
       <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
         <h1 className="h1 mb-6">
-          Land a Piece of the Futeure&nbsp;:&nbsp;Tokenized and Ready to Grow
+          Land a Piece of the Future&nbsp;:&nbsp;Tokenized and Ready to Grow
           with {` `}
           <span className="inline-block relative">
             Zamindar{" "}
